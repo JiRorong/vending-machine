47cbe81580ff00f70c0b558f5a8035df816be531
JiRorong <youhae102@gmail.com>
2022-05-10 01:49:00 +0900

    JS: Load selct items

--- main.js
+++ main.js
@@ -14,24 +14,29 @@ function displayItems(items) {
 function createHTMLItem(item) {
   return `
   <li class="drink">
-    <button class="btns">
+    <button class="btns" data-name="${item.dataset}">
     <img
       src="${item.img}"
       alt="${item.type}"
       class="drink__img"/>
       <span class="drink__name">${item.name}</span>
-      <span class="drink__price">${item.price}</span>
+      <span class="drink__price">${item.price}원</span>
     </button>
 </li>`;
 }
 
 function onButtonCilck(event, items) {
-  const key = event.target.dataset.key;
-  const value = event.target.dataset.value;
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
   if (key == null || value == null) {
     return;
   }
-  displayItems(items.filter((item) => item[key] === value));
+  if (key === '*' && value === '*') {
+    displayItems(items);
+  } else {
+    displayItems(items.filter((item) => item[key] === value));
+  }
 }
 
 function setEventListeners(items) {
@@ -41,10 +46,54 @@ function setEventListeners(items) {
   );
 }
 
+function displaySelect(items) {
+  const mainItem = document.querySelector('.main__item');
+  const SelectList = document.querySelector('.order__list');
+  mainItem.addEventListener('click', (event) => {
+    const drinkName = event.target.dataset.name;
+    if (drinkName == null) {
+      return;
+    }
+    const filter = items.filter(
+      (item) => item.name === drinkName
+    );
+    SelectList.innerHTML += filter
+      .map((item) => asdf(item))
+      .join('');
+  });
+}
+
+function asdf(item) {
+  return `<li class="list">
+  <img
+    src="${item.img}"
+    alt="${item.type}"
+    class="list__img"
+  />
+  <span>${item.name}</span>
+  <span class="drinkValue">${item.price}<span>원</span></span>
+  <div class="option">
+    <input
+      type="text"
+      name="amount"
+      id="amount"
+      class="amount"
+      value="1"
+    />
+    <div class="buttons">
+      <button class="plusBtn">+</button>
+      <button class="minusBtn">-</button>
+    </div>
+    <button class="cancelBtn">x</button>
+  </div>
+</li>`;
+}
+
 loadItems() //
   .then((items) => {
     displayItems(items);
     setEventListeners(items);
+    displaySelect(items);
   });
 
 const chargingBtn = document.querySelector('.chargingBtn');
@@ -66,3 +115,14 @@ chargingBtn.addEventListener('click', () => {
     return;
   }
 });
+
+// ----------------------------
+// const plusBtn = document.querySelector('.plusBtn');
+// const minusBtn = document.querySelector('.minusBtn');
+// const cancelBtn = document.querySelector('.cancelBtn');
+// const drinkValue = document.querySelector('.drinkValue');
+// const amount = document.querySelector('.amount');
+
+// plusBtn.addEventListener('click', () => {
+//   amount.value++;
+// });
