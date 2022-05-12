7e0ea5f012ab4d94f45ecdb6c7de477cf8de6bbe
JiRorong <youhae102@gmail.com>
2022-05-12 16:22:24 +0900

    JS: Select Item, Amount

--- main.js
+++ main.js
@@ -14,7 +14,15 @@ function displayItems(items) {
 function createHTMLItem(item) {
   return `
   <li class="drink">
-    <button class="btns" data-name="${item.dataset}">
+    <button class="btns"
+    data-name="${item.dataset}"
+    data-class="${item.class}"
+    data-plus="${item.plus}"
+    data-minus="${item.minus}"
+    data-cancel="${item.cancel}"
+    data-amount="${item.amount}"
+    data-drinkvalue="${item.drinkValue}"
+    data-price="${item.price}">
     <img
       src="${item.img}"
       alt="${item.type}"
@@ -46,56 +54,18 @@ function setEventListeners(items) {
   );
 }
 
-function displaySelect(items) {
-  const mainItem = document.querySelector('.main__item');
-  const SelectList = document.querySelector('.order__list');
-  mainItem.addEventListener('click', (event) => {
-    const drinkName = event.target.dataset.name;
-    if (drinkName == null) {
-      return;
-    }
-    const filter = items.filter(
-      (item) => item.name === drinkName
-    );
-    SelectList.innerHTML += filter
-      .map((item) => asdf(item))
-      .join('');
-  });
-}
-
-function asdf(item) {
-  return `<li class="list">
-  <img
-    src="${item.img}"
-    alt="${item.type}"
-    class="list__img"
-  />
-  <span>${item.name}</span>
-  <span class="drinkValue">${item.price}<span>원</span></span>
-  <div class="option">
-    <input
-      type="text"
-      name="amount"
-      id="amount"
-      class="amount"
-      value="1"
-    />
-    <div class="buttons">
-      <button class="plusBtn">+</button>
-      <button class="minusBtn">-</button>
-    </div>
-    <button class="cancelBtn">x</button>
-  </div>
-</li>`;
-}
+// function increase() {
+//   const increase = document.querySelectorAll('.list');
+//   console.log(increase);
+// }
 
 loadItems() //
   .then((items) => {
     displayItems(items);
     setEventListeners(items);
-    displaySelect(items);
   });
 
+// 충전
 const chargingBtn = document.querySelector('.chargingBtn');
 chargingBtn.addEventListener('click', () => {
   const getValue = prompt('충전할 금액을 입력하세요.');
@@ -116,13 +86,114 @@ chargingBtn.addEventListener('click', () => {
   }
 });
 
-// ----------------------------
-// const plusBtn = document.querySelector('.plusBtn');
-// const minusBtn = document.querySelector('.minusBtn');
-// const cancelBtn = document.querySelector('.cancelBtn');
-// const drinkValue = document.querySelector('.drinkValue');
-// const amount = document.querySelector('.amount');
+// 선택 아이템
+function plusBtnClick(amount, drinkValue, price, sum) {
+  amount.value++;
+  drinkValue.textContent =
+    parseInt(drinkValue.textContent) + parseInt(price);
+  sum.textContent = parseInt(sum.textContent) + parseInt(price);
+}
 
-// plusBtn.addEventListener('click', () => {
-//   amount.value++;
-// });
+function minusBtnClick(amount, drinkValue, price, sum) {
+  if (amount.value === '1') {
+    return;
+  }
+  amount.value--;
+  drinkValue.textContent =
+    parseInt(drinkValue.textContent) - parseInt(price);
+  sum.textContent = parseInt(sum.textContent) - parseInt(price);
+}
+
+function cancelBtnClick(
+  selectItems,
+  amount,
+  e,
+  price,
+  drinkValue,
+  sum,
+  plusBtn,
+  minusBtn,
+  cancelBtn
+) {
+  sum.textContent =
+    parseInt(sum.textContent) - parseInt(drinkValue.textContent);
+  amount.value = 1;
+  drinkValue.textContent = price;
+  selectItems.style.display = 'none';
+  e.target.style.pointerEvents = 'auto';
+  // 중복된 이벤트 리스너 제거
+  const old_element1 = plusBtn;
+  const new_element1 = plusBtn.cloneNode(true);
+  old_element1.parentNode.replaceChild(
+    new_element1,
+    old_element1
+  );
+
+  const old_element2 = minusBtn;
+  const new_element2 = minusBtn.cloneNode(true);
+  old_element2.parentNode.replaceChild(
+    new_element2,
+    old_element2
+  );
+
+  const old_element3 = cancelBtn;
+  const new_element3 = cancelBtn.cloneNode(true);
+  old_element3.parentNode.replaceChild(
+    new_element3,
+    old_element3
+  );
+}
+
+const asd = document.querySelector('.main__item');
+asd.addEventListener('click', (e) => {
+  const dataset = e.target.dataset;
+  const classs = dataset.class;
+
+  if (classs == null) {
+    return;
+  }
+  // 이벤트 중복 제거
+  e.target.style.pointerEvents = 'none';
+
+  const selectItems = document.querySelector(classs);
+  selectItems.style.display = 'flex';
+
+  const plus = dataset.plus;
+  const minus = dataset.minus;
+  const cancel = dataset.cancel;
+  const amountText = dataset.amount;
+  const drinkValueText = dataset.drinkvalue;
+  const price = dataset.price;
+
+  const plusBtn = document.querySelector(plus);
+  const minusBtn = document.querySelector(minus);
+  const cancelBtn = document.querySelector(cancel);
+  const amount = document.querySelector(amountText);
+  const drinkValue = document.querySelector(drinkValueText);
+
+  const sum = document.querySelector('.sum');
+  sum.textContent =
+    parseInt(sum.textContent) + parseInt(drinkValue.textContent);
+
+  plusBtn.addEventListener('click', () =>
+    plusBtnClick(amount, drinkValue, price, sum)
+  );
+
+  minusBtn.addEventListener('click', () =>
+    minusBtnClick(amount, drinkValue, price, sum)
+  );
+
+  cancelBtn.addEventListener('click', () =>
+    cancelBtnClick(
+      selectItems,
+      amount,
+      e,
+      price,
+      drinkValue,
+      sum,
+      plusBtn,
+      minusBtn,
+      cancelBtn
+    )
+  );
+});
